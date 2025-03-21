7f5d3f315e1d7880066303ca11149e4d1e53d96c
Deborah Koiyan <nushkoiyan@gmail.com>
2025-03-21 22:25:24 +0000

    Added a comment

--- week01/Exercise1/Program1.cs
+++ week01/Exercise1/Program1.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Ask the user for their name.
         Console.WriteLine("What is your first name? ");
         string first = Console.ReadLine();
 
